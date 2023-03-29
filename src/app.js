6819ae940108e5cd79462edf4d7727ad93d72cba
Ulinnaja Aldi <ulinnajaaldi24@gmail.com>
2023-03-29 23:50:12 +0700

    feat: call components for custom html tag

--- src/app.js
+++ src/app.js
@@ -1,7 +1,12 @@
+/* eslint-disable import/extensions */
 import "./style/style.css";
 
 import main from "./script/views/main.js";
+
+// Components
 import "./script/components/navigation-bar.js";
 import "./script/components/hero-element.js";
+import "./script/components/item-list.js";
+import "./script/components/search-item.js";
 
 document.addEventListener("DOMContentLoaded", main);
