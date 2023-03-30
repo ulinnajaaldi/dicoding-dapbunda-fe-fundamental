ee0597fb14048f18fbec2cc56a588eabb77893bc
Ulinnaja Aldi <ulinnajaaldi24@gmail.com>
2023-03-30 23:57:17 +0700

    fix: loader & modal in search meals menu

--- src/script/components/search-item.js
+++ src/script/components/search-item.js
@@ -1,4 +1,3 @@
-/* eslint-disable import/extensions */
 import "./card/card-item.js";
 
 class SearchItem extends HTMLElement {
@@ -6,19 +5,38 @@ class SearchItem extends HTMLElement {
     this.render();
   }
 
-  async render() {
+  render() {
     this.innerHTML = `
-    <section class="flex flex-col items-center justify-center pb-10 gap-4">
-        <h1 class="text-3xl font-bold leading-relaxed">Find Food You Like Here!</h1>
-        <form class="w-[450px] relative">
+    <section class="flex flex-col items-center justify-center pb-10 gap-4 relative" id="searchFood">
+        <h1 class="lg:text-3xl text-xl font-bold leading-relaxed" data-aos="fade-up" data-aos-duration="800" data-aos-once="true">Find Food You Like Here!</h1>
+        <form class="lg:w-[450px] md:w-[400px] w-[350px] relative" data-aos="fade-up" data-aos-duration="800" data-aos-once="true">
                 <div class="absolute inset-y-0 left-0 flex items-center pl-3 z-[99] pointer-events-none">
                     <svg aria-hidden="true" class="w-5 h-5 text-gray-500 dark:text-gray-400" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z"></path></svg>
                 </div>
-                <input id="searchElement" type="search" class="block drop-shadow-lg w-full p-4 pl-10 focus:outline-none rounded-lg border-b" placeholder="Search Food, Menu, Others..." required/>
-                <button type="submit" id="searchButtonElement" class="text-white bg-secondary hover:bg-accent transition-all duration-300 absolute right-2.5 bottom-2.5 font-medium rounded-lg px-4 py-2 z-[99]">Search</button>
+
+                <input id="searchElement" type="search" class="block drop-shadow-lg w-full p-4 pl-10 focus:outline-none rounded-lg border-b md:text-base text-sm " placeholder="Search Food, Menu, Others..." required/>
+                
+                <button type="submit" id="searchButtonElement" class="text-white bg-accent transition-all duration-300 absolute right-2.5 bottom-2.5 font-medium rounded-lg px-4 py-2 z-[99] md:text-base text-sm">
+                  Search
+                </button>
+
+                <div class="hidden justify-center items-center absolute z-[99] right-[38px] top-[18px] bg-accent" id="loaderSearch">
+                    <svg class="animate-spin h-5 w-5 text-white" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24">
+                      <circle class="opacity-25" cx="12" cy="12" r="10" stroke="currentColor" stroke-width="4"></circle>
+                      <path class="opacity-75" fill="currentColor" d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z"></path>
+                    </svg>
+                </div>
         </form>
-        <div class="py-10 grid grid-cols-3 gap-4" id="search-meals-card"></div>
-    </section>`;
+        <div class="md:py-10 py-4 grid md:grid-cols-3 grid-cols-2 md:gap-4 gap-2" id="search-meals-card"></div>
+        
+    </section>
+    <div id="modal" class="modal">
+      <div class="modal-box w-11/12 max-w-6xl max-h-5/6">
+        <label id="close-modal" class="btn btn-sm btn-circle absolute right-2 top-2">✕</label>
+        <p id="meal-details"></p>
+      </div>
+    </div>
+    `;
   }
 }
 
--- src/script/views/main.js
+++ src/script/views/main.js
@@ -1,40 +1,135 @@
-/* eslint-disable no-alert */
-/* eslint-disable no-console */
-/* eslint-disable import/extensions */
-
 import DataSource from "../data/data-source.js";
 
 const main = () => {
   const searchButton = document.querySelector("#searchButtonElement");
   const searchInput = document.querySelector("#searchElement");
   const searchMealsCard = document.querySelector("#search-meals-card");
+  const loader = document.querySelector("#loaderSearch");
 
   searchButton.addEventListener("click", async (e) => {
     e.preventDefault();
+    loader.classList.remove("hidden");
+    loader.classList.add("flex");
+    searchButton.classList.remove("text-white");
+    searchButton.classList.add("text-accent");
     const results = await DataSource.searchMealByName(searchInput.value);
     let mealCards = "";
     if (searchInput.value === "") {
       alert("Please fill the search box!");
-      mealCards = "";
-      searchMealsCard.innerHTML = mealCards;
+      loader.classList.remove("flex");
+      loader.classList.add("hidden");
+      searchButton.classList.remove("text-accent");
+      searchButton.classList.add("text-white");
+      searchMealsCard.innerHTML = "";
     } else if (results && results.length >= 3) {
       results.forEach((result) => {
         mealCards += `<card-item id="${result.idMeal}" src="${result.strMealThumb}" title="${result.strMeal}"></card-item>`;
       });
-      searchMealsCard.classList.add("grid-cols-3", "grid");
+      loader.classList.remove("flex");
+      loader.classList.add("hidden");
+      searchButton.classList.remove("text-accent");
+      searchButton.classList.add("text-white");
+      searchMealsCard.classList.add("md:grid-cols-3", "grid-cols-2", "grid");
       searchMealsCard.classList.remove("flex", "item-center", "justify-center");
       searchMealsCard.innerHTML = mealCards;
+      document.querySelectorAll(".card").forEach((card) => {
+        card.addEventListener("click", async (event) => {
+          const { mealId } = event.currentTarget.dataset;
+          const mealDetails = await DataSource.getMealByID(mealId);
+          let mealDataDetails = "";
+          mealDetails.forEach((meal) => {
+            let ingredientsList = "";
+            for (let i = 1; i <= 20; i++) {
+              if (meal[`strIngredient${i}`]) {
+                ingredientsList += `<p class="p-2 rounded-md transition-all hover:bg-gray-200/60 cursor-pointer">${
+                  meal[`strMeasure${i}`]
+                } ${meal[`strIngredient${i}`]}</p>`;
+              }
+            }
+
+            mealDataDetails += `
+            <h2 class="font-bold lg:text-xl md:text-lg text-base text-primary pb-3">${
+              meal.strMeal
+            }</h2>
+            <div class="flex md:flex-row flex-col items-start justify-start lg:gap-10 gap-5 md:py-1 pb-3 pt-1 border-b-2 md:border-b-0">
+              <img src="${meal.strMealThumb}" alt="${
+              meal.strMeal
+            }" class="lg:h-72 h-56 rounded-xl">
+              <div>
+                <p class="lg:text-lg text-base font-semibold text-primary lg:pb-3 pb-1 pl-2">Ingridient 🍳</p> 
+                <div class="grid lg:grid-rows-6 grid-rows-5 grid-flow-col gap-1 lg:gap-x-3 gap-x-0 lg:text-sm text-xs"> ${ingredientsList}</div>
+              </div>
+            </div>
+            <p class="md:pt-0 pt-3 antialiased leading-relaxed text-justify lg:text-base text-sm whitespace-normal ">
+              ${meal.strInstructions.split("\r\n ")}
+            </p>
+              `;
+          });
+          document.querySelector("#meal-details").innerHTML = mealDataDetails;
+          document.querySelector("#modal").classList.add("modal-open");
+        });
+      });
+      document.querySelector("#close-modal").addEventListener("click", () => {
+        document.querySelector("#modal").classList.remove("modal-open");
+      });
     } else if (results && results.length === 1) {
       results.forEach((result) => {
         mealCards += `<card-item id="${result.idMeal}" src="${result.strMealThumb}" title="${result.strMeal}"></card-item>`;
       });
-      searchMealsCard.classList.remove("grid-cols-3", "grid");
+      loader.classList.remove("flex");
+      loader.classList.add("hidden");
+      searchButton.classList.remove("text-accent");
+      searchButton.classList.add("text-white");
+      searchMealsCard.classList.remove("md:grid-cols-3", "grid-cols-2", "grid");
       searchMealsCard.classList.add("flex", "item-center", "justify-center");
       searchMealsCard.innerHTML = mealCards;
+      document.querySelectorAll(".card").forEach((card) => {
+        card.addEventListener("click", async (event) => {
+          const { mealId } = event.currentTarget.dataset;
+          const mealDetails = await DataSource.getMealByID(mealId);
+          let mealDataDetails = "";
+          mealDetails.forEach((meal) => {
+            let ingredientsList = "";
+            for (let i = 1; i <= 20; i++) {
+              if (meal[`strIngredient${i}`]) {
+                ingredientsList += `<p class="p-2 rounded-md transition-all hover:bg-gray-200/60 cursor-pointer">${
+                  meal[`strMeasure${i}`]
+                } ${meal[`strIngredient${i}`]}</p>`;
+              }
+            }
+
+            mealDataDetails += `
+            <h2 class="font-bold lg:text-xl md:text-lg text-base text-primary pb-3">${
+              meal.strMeal
+            }</h2>
+            <div class="flex md:flex-row flex-col items-start justify-start lg:gap-10 gap-5 md:py-1 pb-3 pt-1 border-b-2 md:border-b-0">
+              <img src="${meal.strMealThumb}" alt="${
+              meal.strMeal
+            }" class="lg:h-72 h-56 rounded-xl">
+              <div>
+                <p class="lg:text-lg text-base font-semibold text-primary lg:pb-3 pb-1 pl-2">Ingridient 🍳</p> 
+                <div class="grid lg:grid-rows-6 grid-rows-5 grid-flow-col gap-1 lg:gap-x-3 gap-x-0 lg:text-sm text-xs"> ${ingredientsList}</div>
+              </div>
+            </div>
+            <p class="md:pt-0 pt-3 antialiased leading-relaxed text-justify lg:text-base text-sm whitespace-normal ">
+              ${meal.strInstructions.split("\r\n ")}
+            </p>
+              `;
+          });
+          document.querySelector("#meal-details").innerHTML = mealDataDetails;
+          document.querySelector("#modal").classList.add("modal-open");
+        });
+      });
+      document.querySelector("#close-modal").addEventListener("click", () => {
+        document.querySelector("#modal").classList.remove("modal-open");
+      });
     } else {
       alert("No results found!");
-      mealCards = "";
-      searchMealsCard.innerHTML = mealCards;
+      loader.classList.remove("flex");
+      loader.classList.add("hidden");
+      searchButton.classList.remove("text-accent");
+      searchButton.classList.add("text-white");
+      searchMealsCard.innerHTML = "";
     }
   });
 };
