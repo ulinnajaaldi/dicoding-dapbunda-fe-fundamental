55e5ae5b4b45e7ede8171f0a54c52bc80515d8bb
Ulinnaja Aldi <ulinnajaaldi24@gmail.com>
2023-03-31 00:00:19 +0700

    feat: add components to app & AOS init

--- src/app.js
+++ src/app.js
@@ -1,12 +1,18 @@
-/* eslint-disable import/extensions */
+/* eslint-disable import/no-extraneous-dependencies */
+import "aos/dist/aos.css";
 import "./style/style.css";
 
+import AOS from "aos";
 import main from "./script/views/main.js";
 
 // Components
 import "./script/components/navigation-bar.js";
 import "./script/components/hero-element.js";
+import "./script/components/about-us.js";
 import "./script/components/item-list.js";
 import "./script/components/search-item.js";
+import "./script/components/footer-component.js";
+
+AOS.init();
 
 document.addEventListener("DOMContentLoaded", main);
